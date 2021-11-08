e787e8c49f65b5d5ae9c248feaf2ad4d0d8a469b
Ivan Severino <i.severino@2hire.io>
2021-11-08 02:38:51 +0100

    init implementation

--- README.md
+++ README.md
@@ -1,6 +1,64 @@
-# Module: Calendar
+# Module: MMM-GoogleCalendar
 
-The `calendar` module is one of the default modules of the MagicMirror.
-This module displays events from a public .ical calendar. It can combine multiple calendars.
+This module is a customization from MaigcMirror's default calendar module to display google calendar events. Inspired by the [GoogleTask module](https://github.com/jgauth/MMM-GoogleTasks).
 
-For configuration options, please check the [MagicMirror² documentation](https://docs.magicmirror.builders/modules/calendar.html).
+### Dependencies
+
+1. The [Google Node.js client library](https://github.com/google/google-api-nodejs-client/): For authentication and Google Calendar API (v3). See Installation for instructions
+
+## Installation
+
+To install the module, use your terminal to:
+
+1. Navigate to your MagicMirror's modules folder. If you are using the default installation directory, use the command:<br />`cd ~/MagicMirror/modules`
+2. Clone the module:<br />`git clone https://github.com/randomBrainstormer/MMM-GoogleCalendar.git`
+3. Install Google API:<br />`npm install googleapis`
+
+## Authentication Setup
+
+Before you can add your calendar you need to setup the Google Calendar API and OAuth2 client from the Google Cloud Platform:
+
+1. Go [here](https://developers.google.com/calendar/api/quickstart/nodejs), and follow the instructions found in the `prerequisites` to create the Google Cloud project (you could also use an existing project if you wish).
+2. Once you have enabled setup the project and created your OAuth ID client, download the client ID as `json` (look for the download option) and rename it `credentials.json`.
+3. Move `credentials.json` to your MMM-GoogleCalendar directory (MagicMirror/modules/MMM-GoogleCalendar/)
+4. [Enable Google Calendar API](https://console.cloud.google.com/apis/library/calendar-json.googleapis.com). Select the same project as in step 1.
+5. Run authenticate.js:<br />`node authenticate.js`.
+6. Follow the instructions and it should print your calendar. Copy the ID of the calendar you want to the config.
+
+## Using the module
+
+### MagicMirror² Configuration
+
+To use this module, add the following configuration block to the modules array in the `config/config.js` file:
+
+```js
+var config = {
+    modules: [
+        ...
+        {
+            module: 'MMM-GoogleCalendar',
+            header: "My Google Cal",
+            position: "top_left",
+            config: {
+              calendars: [
+                {
+                  symbol: "calendar-week",
+                  calendarID: "GoogleCalID"
+                },
+                {
+                  symbol: "calendar-alt",
+                  calendarID: "YourGoogleCalID"
+                },
+              ],
+              ...
+                // See below for configuration options
+            }
+        },
+        ...
+    ]
+}
+```
+
+### Configuration Options
+
+Work in progress. PRs are welcome. Most of the options from the original calendar module are implemented, please check the [MagicMirror² documentation](https://docs.magicmirror.builders/modules/calendar.html).
--- node_helper.js
+++ node_helper.js
@@ -1,88 +1,166 @@
-/* Magic Mirror
- * Node Helper: Calendar
- *
- * By Michael Teeuw https://michaelteeuw.nl
- * MIT Licensed.
- */
 const NodeHelper = require("node_helper");
-const CalendarFetcher = require("./calendarfetcher.js");
+const { google } = require("googleapis");
+const fs = require("fs");
 const Log = require("logger");
 
+const TOKEN_PATH = "/token.json";
+
 module.exports = NodeHelper.create({
-	// Override start method.
-	start: function () {
-		Log.log("Starting node helper for: " + this.name);
-		this.fetchers = [];
-	},
+  // Override start method.
+  start: function () {
+    Log.log("Starting node helper for: " + this.name);
+    this.fetchers = [];
+    this.isHelperActive = true;
 
-	// Override socketNotificationReceived method.
-	socketNotificationReceived: function (notification, payload) {
-		if (notification === "ADD_CALENDAR") {
-			this.createFetcher(payload.url, payload.fetchInterval, payload.excludedEvents, payload.maximumEntries, payload.maximumNumberOfDays, payload.auth, payload.broadcastPastEvents, payload.selfSignedCert, payload.id);
-		}
-	},
+    this.calendarService;
+  },
 
-	/**
-	 * Creates a fetcher for a new url if it doesn't exist yet.
-	 * Otherwise it reuses the existing one.
-	 *
-	 * @param {string} url The url of the calendar
-	 * @param {number} fetchInterval How often does the calendar needs to be fetched in ms
-	 * @param {string[]} excludedEvents An array of words / phrases from event titles that will be excluded from being shown.
-	 * @param {number} maximumEntries The maximum number of events fetched.
-	 * @param {number} maximumNumberOfDays The maximum number of days an event should be in the future.
-	 * @param {object} auth The object containing options for authentication against the calendar.
-	 * @param {boolean} broadcastPastEvents If true events from the past maximumNumberOfDays will be included in event broadcasts
-	 * @param {boolean} selfSignedCert If true, the server certificate is not verified against the list of supplied CAs.
-	 * @param {string} identifier ID of the module
-	 */
-	createFetcher: function (url, fetchInterval, excludedEvents, maximumEntries, maximumNumberOfDays, auth, broadcastPastEvents, selfSignedCert, identifier) {
-		try {
-			new URL(url);
-		} catch (error) {
-			Log.error("Calendar Error. Malformed calendar url: ", url, error);
-			this.sendSocketNotification("CALENDAR_ERROR", { error_type: "MODULE_ERROR_MALFORMED_URL" });
-			return;
-		}
+  stop: function () {
+    this.isHelperActive = false;
+  },
 
-		let fetcher;
-		if (typeof this.fetchers[identifier + url] === "undefined") {
-			Log.log("Create new calendarfetcher for url: " + url + " - Interval: " + fetchInterval);
-			fetcher = new CalendarFetcher(url, fetchInterval, excludedEvents, maximumEntries, maximumNumberOfDays, auth, broadcastPastEvents, selfSignedCert);
+  // Override socketNotificationReceived method.
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === "MODULE_READY") {
+      if (!this.calendarService) {
+        this.authenticate();
+      } else {
+        this.sendSocketNotification("SERVICE_READY", {});
+      }
+    }
+    if (notification === "ADD_CALENDAR") {
+      this.createFetcher(
+        payload.calendarID,
+        payload.fetchInterval,
+        payload.maximumEntries,
+        payload.id
+      );
+    }
+  },
 
-			fetcher.onReceive((fetcher) => {
-				this.broadcastEvents(fetcher, identifier);
-			});
+  // Authenticate oAuth credentials
+  authenticate: function () {
+    var _this = this;
 
-			fetcher.onError((fetcher, error) => {
-				Log.error("Calendar Error. Could not fetch calendar: ", fetcher.url(), error);
-				let error_type = NodeHelper.checkFetchError(error);
-				this.sendSocketNotification("CALENDAR_ERROR", {
-					id: identifier,
-					error_type
-				});
-			});
+    fs.readFile(_this.path + "/credentials.json", (err, content) => {
+      if (err) {
+        _this.sendSocketNotification("AUTH_FAILED", { error_type: err });
+        return console.log("Error loading client secret file:", err);
+      }
+      // Authorize a client with credentials, then call the Google Tasks API.
+      authorize(JSON.parse(content), _this.startCalendarService);
+    });
 
-			this.fetchers[identifier + url] = fetcher;
-		} else {
-			Log.log("Use existing calendarfetcher for url: " + url);
-			fetcher = this.fetchers[identifier + url];
-			fetcher.broadcastEvents();
-		}
+    /**
+     * Create an OAuth2 client with the given credentials, and then execute the
+     * given callback function.
+     * @param {Object} credentials The authorization client credentials.
+     * @param {function} callback The callback to call with the authorized client.
+     */
+    function authorize(credentials, callback) {
+      const { client_secret, client_id, redirect_uris } = credentials.installed;
+      _this.oAuth2Client = new google.auth.OAuth2(
+        client_id,
+        client_secret,
+        redirect_uris[0]
+      );
 
-		fetcher.startFetch();
-	},
+      // Check if we have previously stored a token.
+      fs.readFile(_this.path + TOKEN_PATH, (err, token) => {
+        if (err) {
+          return console.log(
+            this.name + ": Error loading token:",
+            err,
+            "Make sure you have authorized the app."
+          );
+        }
+        _this.oAuth2Client.setCredentials(JSON.parse(token));
+        callback(_this.oAuth2Client, _this);
+      });
+    }
+  },
 
-	/**
-	 *
-	 * @param {object} fetcher the fetcher associated with the calendar
-	 * @param {string} identifier the identifier of the calendar
-	 */
-	broadcastEvents: function (fetcher, identifier) {
-		this.sendSocketNotification("CALENDAR_EVENTS", {
-			id: identifier,
-			url: fetcher.url(),
-			events: fetcher.events()
-		});
-	}
+  startCalendarService: function (auth, _this) {
+    _this.calendarService = google.calendar({ version: "v3", auth });
+    _this.sendSocketNotification("SERVICE_READY", {});
+  },
+
+  /**
+   * Fetch calendars
+   *
+   * @param {string} calendarID The ID of the calendar
+   * @param {number} fetchInterval How often does the calendar needs to be fetched in ms
+   * @param {number} maximumEntries The maximum number of events fetched.
+   * @param {string} identifier ID of the module
+   */
+  fetchCalendar: function (
+    calendarID,
+    fetchInterval,
+    maximumEntries,
+    identifier
+  ) {
+    this.calendarService.events.list(
+      {
+        calendarId: calendarID,
+        timeMin: new Date().toISOString(),
+        maxResults: maximumEntries,
+        singleEvents: true,
+        orderBy: "startTime"
+      },
+      (err, res) => {
+        if (err) {
+          Log.error(
+            "Calendar Error. Could not fetch calendar: ",
+            calendarID,
+            err
+          );
+          let error_type = NodeHelper.checkFetchError(err);
+          this.sendSocketNotification("CALENDAR_ERROR", {
+            id: identifier,
+            error_type
+          });
+          return;
+        }
+
+        const events = res.data.items;
+        Log.info(
+          `${this.name}: ${events.length} events loaded for ${calendarID}`
+        );
+        this.broadcastEvents(events, identifier, calendarID);
+        this.scheduleNextCalendarFetch(
+          calendarID,
+          fetchInterval,
+          maximumEntries,
+          identifier
+        );
+      }
+    );
+  },
+
+  scheduleNextCalendarFetch: function (
+    calendarID,
+    fetchInterval,
+    maximumEntries,
+    identifier
+  ) {
+    var _this = this;
+    if (this.isHelperActive) {
+      setTimeout(function () {
+        _this.fetchCalendar(
+          calendarID,
+          fetchInterval,
+          maximumEntries,
+          identifier
+        );
+      }, fetchInterval);
+    }
+  },
+
+  broadcastEvents: function (events, identifier, calendarID) {
+    this.sendSocketNotification("CALENDAR_EVENTS", {
+      id: identifier,
+      calendarID,
+      events: events
+    });
+  }
 });
