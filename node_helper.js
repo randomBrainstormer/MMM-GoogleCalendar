29e4209963b4721d50a58e2475d8f2e5206ac960
Ivan Severino <i.severino@2hire.io>
2021-11-08 03:03:34 +0100

    fix: calendar not starting

--- node_helper.js
+++ node_helper.js
@@ -29,7 +29,7 @@ module.exports = NodeHelper.create({
       }
     }
     if (notification === "ADD_CALENDAR") {
-      this.createFetcher(
+      this.fetchCalendar(
         payload.calendarID,
         payload.fetchInterval,
         payload.maximumEntries,
